613ce6522e975c5650eaf06b5ff47fc8de429b2b
Fabrice M <amonkefabrice@gmail.com>
2024-11-11 03:13:38 +0100

    All commit

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,78 @@
+import { useEffect, useState,Fragment } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  const [isManual, setIsManual] = useState(false); // Nouveau state pour suivre l'interaction manuelle
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+  );
+ 
+  const nextCard = () => {
+// La condition vérifie si byDateDesc est défini avant d'exécuter nextCard
+    if (byDateDesc && !isManual) {
+     const timer = setTimeout(
+        // length - 1 après l'affichage de la dernière image, l'index sera ramené à 0
+        () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+        5000
+      );
+      return timer;  // il renvoie l'ID du timeout depuis la fonction nextCard
+    }
+    return null;  // Ajoutez un retour par défaut si byDateDesc n'est pas défini
+  };
+
+  useEffect(() => {
+    const timer = nextCard(); // Démarre la fonction qui change les cartes
+    return () => clearTimeout(timer); // Nettoie le timeout quand le composant se démonte
+  }, [index, byDateDesc, isManual]); // Ajout index, byDateDesc et isManual comme dépendances pour s'assurer que nextCard n'est appelée que lorsque c'est nécessaireet pas de façon continuelle.
+   // Cette fonction est appelée lorsque l'utilisateur clique sur un bouton radio
+  const handleRadioChange = (radioIdx) => {
+    setIsManual(true); // Indique que l'utilisateur est en interaction manuelle
+    setIndex(radioIdx); // Met immédiatement à jour l'index
+    setTimeout(() => setIsManual(false), ); 
+  };
+  if (!byDateDesc) return null;
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        // ajout d'une key unique au composant Fragment
+        <Fragment key={event.title}>
+          <div 
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((_, radioIdx) => (
+                <input
+                // ajout d'un key unique 
+                  key={_.date} 
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                  onChange={() => handleRadioChange(radioIdx)} // Change d'index quand un bouton est sélectionné
+                />
+              ))}
+            </div>
+          </div>
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,70 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
+};
+
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+   const [lastEvent, setlastEvent] = useState(null);  // Création d'un state pour stocker le dernier événement.
+  const getData = useCallback(async () => {
+    try {
+      
+       // Appel à la méthode loadData de l'objet api pour charger les données
+       const apiData= await api.loadData();
+
+       // Stockage des données
+       setData(apiData);
+ 
+       // Calcul du dernier événement en triant les événements par date 
+       // et en récupérant le premier élément de la liste triée (qui est le dernier événement).
+       const last = apiData?.events.sort((evtA, evtB) =>
+         new Date(evtB.date) < new Date(evtA.date)? -1 : 1)[0];
+ 
+       // Stockage des données
+       setlastEvent(last)
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+  useEffect(() => {
+    if (data) return;
+    getData();
+  });
+  
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        lastEvent,
+        // Utilisation du contexte pour fournir les données
+        //  le dernier événement (lastEvent) aux composants enfants via la propriété value
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
